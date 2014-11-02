cf07cc1a8cc8e3136acc5517495a64c2c8812666
Takahiro Ikeuchi <takahiro_ikeuchi@albert2005.co.jp>
2014-11-02 18:02:36 +0900

    Modified merge miss

--- setup.py
+++ setup.py
@@ -8,11 +8,7 @@ __author__ = 'Takahiro Ikeuchi'
 
 setup(
     name="slackpy",
-<<<<<<< HEAD
-    version="0.0.5",
-=======
     version="0.1.0",
->>>>>>> release/0.1.0
     py_modules=['slackpy'],
     package_dir={'': 'slackpy'},
     install_requires=open('requirements.txt').read().splitlines(),
