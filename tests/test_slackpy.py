4963f2a6d05cbb908f096dc247b4395310d3fa4e
Takahiro Ikeuchi <takahiro_ikeuchi@Takahiro-no-iMac.local>
2015-05-26 19:15:59 +0900

    Add UnitTest

---
+++ tests/test_slackpy.py
@@ -0,0 +1,30 @@
+__author__ = 'takahiro_ikeuchi'
+
+from slackpy.slackpy import SlackLogger
+
+
+class TestSlackLogger:
+
+    def pytest_funcarg__logger(self):
+
+        return SlackLogger('http://dummy_url', '#dummy_channel', 'Test User')
+
+    def test_construct_payload(self, logger):
+
+        expected = logger._SlackLogger__construct_payload('Test Message', 'Test Title', 'Color Name')
+
+        actual = {
+            "channel": "#dummy_channel",
+            "username": "Test User",
+            "attachments":
+                {
+                    "fields": {
+                        "title": "Test Title",
+                        "text": "Test Message",
+                        "color": "Color Name",
+                        "fallback": "Test Title",
+                    }
+                }
+        }
+
+        assert expected == actual
