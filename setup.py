f132f14d60a60bb2af89ba1c1d4b0c31cff68b6f
iktakahiro <takahiro.ikeuchi@gmail.com>
2015-08-18 23:10:38 +0900

    Change version 1.1.2 to 1.1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ __author__ = 'Takahiro Ikeuchi'
 
 setup(
     name="slackpy",
-    version="1.1.2",
+    version="1.1.3",
     py_modules=['slackpy'],
     package_dir={'': 'slackpy'},
     install_requires=open('requirements.txt').read().splitlines(),
