35571f9111cee2fde6a5f81f37d7744721cd0bbb
iktakahiro <takahiro.ikeuchi@gmail.com>
2015-12-07 02:02:33 +0900

    Add unit test for set_log_level().

--- tests/test_slackpy.py
+++ tests/test_slackpy.py
@@ -1,18 +1,18 @@
-__author__ = 'takahiro_ikeuchi'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import pytest
 from slackpy.slackpy import SlackLogger
 
+__author__ = 'takahiro_ikeuchi'
+
 
 class TestSlackLogger:
-
     def test_channel_value_error(self):
-
         with pytest.raises(ValueError):
             SlackLogger('http://dummy_url', 'dummy_channel', 'Test User')
 
     def test_build_payload_with_all_parameters(self):
-
         logger = SlackLogger('http://dummy_url', '#dummy_channel', 'Test User')
         actual = logger._SlackLogger__build_payload('Test Message',
                                                     'Test Title',
@@ -37,7 +37,6 @@ class TestSlackLogger:
         assert expected == actual
 
     def test_build_payload_without_specifying_optional_parameters(self):
-
         logger = SlackLogger('http://dummy_url')
         actual = logger._SlackLogger__build_payload('Test Message',
                                                     'Test Title',
@@ -62,7 +61,6 @@ class TestSlackLogger:
         assert expected == actual
 
     def test_build_payload_with_custom_fields(self):
-
         logger = SlackLogger('http://dummy_url', '#dummy_channel', 'Test User')
 
         test_fields = []
@@ -104,3 +102,22 @@ class TestSlackLogger:
         }
 
         assert expected == actual
+
+    def test_to_set_valid_value_log_level(self):
+        logger = SlackLogger('http://dummy_url')
+
+        for lv in [10, 20, 30, 40]:
+            logger.set_log_level(lv)
+            assert logger.log_level == lv
+
+    def test_to_set_invalid_value_log_level(self):
+        logger = SlackLogger('http://dummy_url')
+
+        with pytest.raises(ValueError):
+            logger.set_log_level(99)
+
+        with pytest.raises(ValueError):
+            logger.set_log_level(0)
+
+        with pytest.raises(TypeError):
+            logger.set_log_level()
